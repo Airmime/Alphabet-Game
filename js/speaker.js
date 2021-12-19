857e05d9064e7bfcf67bd6c7fc8999a8444515ce
Rémi MARION <r.marion42@hotmail.fr>
2021-12-19 10:53:27 +0100

    Fix speaker rate.

--- js/speaker.js
+++ js/speaker.js
@@ -5,6 +5,5 @@
 function useSpeaker(whatISay) {
   let synth = window.speechSynthesis;
   var wahtIspeak = new SpeechSynthesisUtterance(whatISay);
-  wahtIspeak.rate = 1.7;
   synth.speak(wahtIspeak);
 }
